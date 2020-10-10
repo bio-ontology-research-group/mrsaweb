d646cf0600fb2d32ec19cf9981fc626d06cfb117
Ali R. Syed <ali.syed@kaust.edu.sa>
2020-10-11 00:27:45 +0300

    added handling submissions failed to upload

--- mrsaweb/apps/uploader/tasks.py
+++ mrsaweb/apps/uploader/tasks.py
@@ -18,41 +18,45 @@ from mrsaweb.virtuoso import insert
 
 @task
 def upload_to_arvados(upload_pk, sequence_read_1, sequence_read_2, metadata_file):
-    logger = upload_to_arvados.get_logger()
-    api = arvados.api(host=settings.ARVADOS_API_HOST, token=settings.ARVADOS_API_TOKEN)
-    col = arvados.collection.Collection(api_client=api)
-    
-    metadata = yaml.load(open(metadata_file), Loader=yaml.FullLoader)
-    qc_fasta(sequence_read_1)
-    qc_fasta(sequence_read_2)
-
-    upload_file(col, sequence_read_1, 'reads1.fastq.gz')
-    upload_file(col, sequence_read_2, 'reads2.fastq.gz')
-    upload_file(col, metadata_file, 'metadata.yaml')
-    external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-
     try:
-        username = getpass.getuser()
-    except KeyError:
-        username = "unknown"
+        logger = upload_to_arvados.get_logger()
+        api = arvados.api(host=settings.ARVADOS_API_HOST, token=settings.ARVADOS_API_TOKEN)
+        col = arvados.collection.Collection(api_client=api)
+        
+        metadata = yaml.load(open(metadata_file), Loader=yaml.FullLoader)
+        qc_fasta(sequence_read_1)
+        qc_fasta(sequence_read_2)
 
-    properties = {
-        "sequence_label": metadata['sample']['sample_id'],
-        "upload_app": "mrsa-uploader",
-        "upload_ip": external_ip,
-        "upload_user": "%s@%s" % (username, socket.gethostname())
-    }
-    logger.info("%s uploaded by %s from %s", metadata['sample']['sample_id'], properties['upload_user'], properties['upload_ip'])
-    result = col.save_new(owner_uuid=settings.UPLOAD_PROJECT, name="%s uploaded by %s from %s" %
-                 (metadata['sample']['sample_id'], properties['upload_user'], properties['upload_ip']),
-                 properties=properties, ensure_unique_name=True)
-    response = col.api_response()
-    update_upload_success(upload_pk, response['uuid'])
-    update_rdfstore(response['uuid'], metadata_file)
+        upload_file(col, sequence_read_1, 'reads1.fastq.gz')
+        upload_file(col, sequence_read_2, 'reads2.fastq.gz')
+        upload_file(col, metadata_file, 'metadata.yaml')
+        external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
-    os.remove(sequence_read_1)
-    os.remove(sequence_read_2)
-    os.remove(metadata_file)
+        try:
+            username = getpass.getuser()
+        except KeyError:
+            username = "unknown"
+
+        properties = {
+            "sequence_label": metadata['sample']['sample_id'],
+            "upload_app": "mrsa-uploader",
+            "upload_ip": external_ip,
+            "upload_user": "%s@%s" % (username, socket.gethostname())
+        }
+        logger.info("%s uploaded by %s from %s", metadata['sample']['sample_id'], properties['upload_user'], properties['upload_ip'])
+        result = col.save_new(owner_uuid=settings.UPLOAD_PROJECT, name="%s uploaded by %s from %s" %
+                    (metadata['sample']['sample_id'], properties['upload_user'], properties['upload_ip']),
+                    properties=properties, ensure_unique_name=True)
+        response = col.api_response()
+        update_upload_success(upload_pk, response['uuid'])
+        update_rdfstore(response['uuid'], metadata_file)
+    except Exception as e:
+        logger.exception("message")
+        update_upload_failed(update_upload_failed, e)
+    finally:
+        os.remove(sequence_read_1)
+        os.remove(sequence_read_2)
+        os.remove(metadata_file)
 
 
 def update_upload_failed(upload_pk, error_message):
