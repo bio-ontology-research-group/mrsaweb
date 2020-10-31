4a2ffcd363366f27af602cefdc7b8b8497a82428
Ali R. Syed <ali.syed@kaust.edu.sa>
2020-11-01 01:49:29 +0300

    galaxy integration

---
+++ mrsaweb/apps/uploader/galaxy.py
@@ -0,0 +1,35 @@
+
+import urllib
+import os
+import logging
+
+from bioblend.galaxy import GalaxyInstance
+
+from django.conf import settings
+
+logger = logging.getLogger(__name__)
+gi = GalaxyInstance(settings.GALAXY_API_BASEURL, key=settings.GALAXY_API_KEY)
+
+tmpdir = '/tmp'
+
+def upload(file_url, base_folder_id):
+    try:
+        filename = file_url.rsplit('/', 1)[1]
+        f = urllib.request.urlopen(file_url)
+        tmpfile = os.path.join(tmpdir, filename)
+        with open(tmpfile, 'wb+') as tmp:
+            tmp.write(f.read())
+
+        result = gi.libraries.upload_file_from_local_path(settings.LIBRARY_ID, tmpfile, folder_id=base_folder_id)
+        logger.info('uploaded: %s', str(result))
+        return result
+    except Exception as e:
+        logger.exception("message")
+        raise Exception(e)
+    finally: 
+        os.remove(tmpfile)
+
+def create_folder(name):
+    result = gi.libraries.create_folder(settings.LIBRARY_ID, name)
+    logger.info('folder created: %s', result[0]['id'])
+    return result[0]['id']
